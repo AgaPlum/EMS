e73f9238ea3cdf5d1f896bcb09bfc77fe1607295
Aga Plum <agaplum@gmail.com>
2016-05-17 09:50:42 +0200

    newap

--- MyC/Exercise/1/ex1_4.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(void)/*;*/
-{
-printf("Keep looking!");
-printf("You\'ll find it!\ns");
-return 0;
-}
--- MyC/Exercise/1/ex1_5.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(void)
-{
-printf("This is a program with a");
-/*do_it*/printf("problem!");
-return 0;
-}
